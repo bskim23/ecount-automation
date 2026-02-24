7dca54641139c084ba32cef1d19e910a0743c143
bskim23 <150322753+bskim23@users.noreply.github.com>
2026-02-24 09:56:48 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+A sad unicorn
+Sorry, this is just a placeholder…
+Continuous deployment has been set up, but your code is not yet deployed.
+
+This revision is a placeholder until your code successfully builds and deploys to the Cloud Run service ecount-automation in asia-northeast3 of the GCP project ecount-automation-488400.
+
+What's next?
+Open the Cloud Run service page.
+Check the status of your deployment.
+If the build has failed, click "Build History".
+Examine your build logs to understand why it failed.
+Fix the issue in your code or Dockerfile (if any).
+Commit and push the change to your repository.
+BACK TO CLOUD RUN
