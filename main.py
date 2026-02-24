7c3d4611aa8194fd4aa1abba2b796541e3596ebc
bskim23 <150322753+bskim23@users.noreply.github.com>
2026-02-24 10:26:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ Examine your build logs to understand why it failed.
 Fix the issue in your code or Dockerfile (if any).
 Commit and push the change to your repository.
 BACK TO CLOUD RUN
-
+# trigger build
